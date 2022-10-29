e63c2a59cea4d5ff69f35f346570aba78bd4ab01
NatchanonKmutnb <s6301012620031@email.kmutnb.ac.th>
2022-10-29 16:45:09 +0700

    Git push 1

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,8 +13,10 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the movies page$/ then '/movies'
-      
+    
+    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the home\s?page$/ then '/movies'
+    when /^the Create New Movie page/ then '/movies/new'  
       # Here is an example that uses value from the Regexp:
       #
       #   when /^the details page for movie "(.*)"$/ do |movie_name|
